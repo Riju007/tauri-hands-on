d563a695306356aafb2095090e02fcb131c1da46
Riju <avisekdey1994@gmail.com>
2026-01-13 21:42:13 +0530

    Created basic snippet function

--- snippet-stash/src/main.js
+++ snippet-stash/src/main.js
@@ -1,18 +1,23 @@
+// import {invoke} from "@tauri-apps/api/core";
 const { invoke } = window.__TAURI__.core;
 
-let greetInputEl;
-let greetMsgEl;
+// 1. select dom element
+const fileNameInput = document.getElementById("filename-input");
+const contentInput = document.getElementById("content-input");
+const saveBtn = document.getElementById("save-btn");
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
+// 2. add the click listener
+saveBtn.addEventListener("click", async () => {
+    const name = fileNameInput.value;
+    const content = contentInput.value;
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+    // 3. call the rust backend
+    // The first argument "save_snippet" matches the function name is lib.rs
+    // the second argument is an object where keys match the rust arguments
+    await invoke("save_snippet", {name: name, content: content});
+
+    // optional: clear the inputs to show it "worked"
+    console.log("Sent to rust!");
+    fileNameInput.value = "";
+    contentInput.value = "";
+})
