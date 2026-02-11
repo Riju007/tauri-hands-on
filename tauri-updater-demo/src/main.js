9b29d186d1fa397f7be054324e0f5cfa84cfd9dc
Riju <avisekdey1994@gmail.com>
2026-02-12 01:39:46 +0530

    Added version display feature

--- tauri-updater-demo/src/main.js
+++ tauri-updater-demo/src/main.js
@@ -2,10 +2,18 @@ const { invoke } = window.__TAURI__.core;
 
 let greetInputEl;
 let greetMsgEl;
+let versionBtn;
+let versionData;
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+async function get_version() {
+  let version = await invoke("get_version");
+  console.log(`Version is: ${version}`);
+  versionData.textContent = version;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -15,4 +23,12 @@ window.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     greet();
   });
+  versionBtn = document.querySelector("#version-button");
+  versionData = document.querySelector("#version-data");
+  versionBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    get_version();
+  });
 });
+
+// version code
